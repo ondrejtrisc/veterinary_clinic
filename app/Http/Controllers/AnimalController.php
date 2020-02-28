596d6480dce7eb6e0336ca92312a3e3a68d664cc
ondrejtrisc <57498031+ondrejtrisc@users.noreply.github.com>
2020-02-28 11:42:48 +0100

    animal controller

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class AnimalController extends Controller
 {
-    //
+    public function show($id)
+    {
+        $animal = Animal::findOrFail($id);
+
+        return view('admin/animal/show', compact('animal'));
+    }
 }
